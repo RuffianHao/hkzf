ffe01f70cea2a9a05e5ff97c79096aa317547e05
JokerHao <1240761013@qq.com>
2019-12-20 11:44:47 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+/*
+ * @Description: 
+ * @Date: 2019-12-20 10:39:07
+ * @LastEditTime : 2019-12-20 11:40:36
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
     </div>
   );
 }
