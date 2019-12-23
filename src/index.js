58f3d349fff470eb77ab097fe21c9207f5fd73d0
JokerHao <1240761013@qq.com>
2019-12-23 17:13:25 +0800

    城市列表

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /*
  * @Description:
  * @Date: 2019-12-20 10:39:07
- * @LastEditTime : 2019-12-20 15:29:45
+ * @LastEditTime : 2019-12-23 14:38:58
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'react-virtualized/styles.css'
 import 'antd-mobile/dist/antd-mobile.css'
 // 导入字体图标库的样式文件
 import './assets/fonts/iconfont.css'
--- src/pages/Citylist/index.jsx
+++ src/pages/Citylist/index.jsx
@@ -1,65 +1,208 @@
 import React from 'react'
+
 import request from '../../utils/axios'
+
 import { getCurrCity } from '../../utils/getCity'
+
+// 导入 List 组件
+import { List, AutoSizer } from 'react-virtualized'
+
+import { Toast } from 'antd-mobile'
+
+import NavHeader from '../../components/NavHeader'
+
+import { setCity } from '../../utils/isCity'
+
+import './index.css'
 // 设计城市的格式
 //已有数据:{label: "东莞", value: "AREA|df4995a8-a691-8898", pinyin: "dongguan", short: "dg"}
 // 需要格式:a:[{label:....},{}]
-
 function formatCityData (data) {
-  let citylist = {}
+  let cityList = {}
   let cityIndex = []
   data.map(item => {
     // 获取简写中的首字母进行分类
     const first = item.short.substr(0, 1)// a,b,c...
     // 判断数组中是否存在属性
-    if (first in citylist) {
-      citylist[first].push(item)
+    if (first in cityList) {
+      cityList[first].push(item)
     } else {
-      citylist[first] = [item]
+      cityList[first] = [item]
     }
     // 取出所有的key a,b,c 使用sort 进行排序
-    cityIndex = Object.keys(citylist).sort()
+    cityIndex = Object.keys(cityList).sort()
   })
   return {
-    citylist,
+    cityList,
     cityIndex
   }
 }
 
-export default class News extends React.Component {
-  state = {
-    cities: []
+
+
+// 索引（A、B等）的高度
+const TITLE_HEIGHT = 36
+// 每个城市名称的高度
+const NAME_HEIGHT = 50
+
+// 封装处理字母索引的方法
+const formatCityIndex = letter => {
+  switch (letter) {
+    case '#':
+      return '当前位置'
+    case 'hot':
+      return '热门位置'
+    default:
+      return letter.toUpperCase()
   }
+
+}
+
+// 有房源的城市
+const HOUSE_CITY = ['北京', '上海', '广州', '深圳']
+
+
+export default class News extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cityList: {},
+      cityIndex: [],
+      // 指定右侧字母索引列表高亮的索引号
+      activeIndex: 0
+    }
+    // 创建ref对象
+    this.cityListComponent = React.createRef()
+    console.log(this.cityListComponent)
+  }
+
+  async componentDidMount () {
+    await this.getCities()
+    // 调用 measureAllRows，提前计算 List 中每一行的高度，实现 scrollToRow 的精确跳转
+    this.cityListComponent.current.measureAllRows()
+    console.log(this.cityListComponent)
+  }
+
+
+
   // 获取城市列表信息
   getCities = async () => {
-    const res = await request.get(`/area/city`, {
+    const res = await request.get('/area/city', {
       params: {
         level: 1 // url参数 level为1->表示一级城市
       }
     })
-    this.setState({
-      cities: res.data.body
-    })
-    const { cityIndex, citylist } = formatCityData(res.data.body)
-    // 热门城市
-    const hotres = await request.get(`/area/hot`)
-    // console.log(res2.data.body)
-    cityIndex.unshift('hot')
-    citylist['hot'] = hotres.data.body
-    // 获取当前定位城市数据
-    const currCity = await getCurrCity()
-    cityIndex.unshift('#')
-    citylist['#'] = [currCity]
-    console.log(cityIndex)
-  }
-  componentDidMount () {
-    this.getCities()
 
+    const { cityIndex, cityList } = formatCityData(res.data.body)
+    // 获取热门城市数据
+    const hotRes = await request.get('/area/hot')
+    cityList['hot'] = hotRes.data.body
+    cityIndex.unshift('hot')
+
+    // 获取当前定位城市
+    const curCity = await getCurrCity()
+    cityList['#'] = [curCity]
+    cityIndex.unshift('#')
+    this.setState({
+      cityList,
+      cityIndex
+    })
   }
-  render () {
+
+  changeCity ({ label, value }) {
+    if (HOUSE_CITY.indexOf(label) > -1) {
+      // 有
+      setCity({ label, value })
+      this.props.history.go(-1)
+    } else {
+      Toast.info('该城市暂无房源数据', 1, null, false)
+    }
+  }
+
+
+  rowRenderer = ({ key, index, isScrolling, isVisible, style }) => {
+    const { cityIndex, cityList } = this.state
+    const letter = cityIndex[index]
     return (
-      <div style={{ backgroundColor: 'green' }}>
-        这是 News组件 的内容，它是子路由的内容
+      <div key={key} style={style} className="city">
+        <div className="title">{formatCityIndex(letter)}</div>
+        {cityList[letter].map(item => (
+          <div
+            className="name"
+            key={item.value}
+            onClick={() => this.changeCity(item)}
+          >
+            {item.label}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  // 创建动态计算每一行高度的方法
+  getRowHeight = ({ index }) => {
+    const { cityList, cityIndex } = this.state
+    // 索引标题高度 + 城市数量 * 城市名称的高度
+    return TITLE_HEIGHT + cityList[cityIndex[index]].length * NAME_HEIGHT
+  }
+
+  // 封装渲染右侧索引列表的方法
+  renderCityIndex () {
+    // 获取到 cityIndex，并遍历其，实现渲染
+    const { cityIndex, activeIndex } = this.state
+    return cityIndex.map((item, index) => (
+      <li
+        className="city-index-item"
+        key={item}
+        onClick={() => {
+          this.cityListComponent.current.scrollToRow(index)
+          console.log('当前索引号：', index)
+        }}
+      >
+        <span className={activeIndex === index ? 'index-active' : ''}>
+          {item === 'hot' ? '热' : item.toUpperCase()}
+        </span>
+      </li>
+    ))
+  }
+
+  // 用于获取List组件中渲染行的信息
+  onRowsRendered = ({ startIndex }) => {
+    console.log('startIndex：', startIndex)
+    if (this.state.activeIndex !== startIndex) {
+      this.setState({
+        activeIndex: startIndex
+      })
+    }
+  }
+
+
+  render () {
+    const { state } = this
+    return (
+      <div className="citylist">
+        {/* 顶部导航栏 */}
+        <NavHeader>城市选择</NavHeader>
+
+        {/* 城市列表 */}
+        <AutoSizer>
+          {({ width, height }) => (
+            <List
+              ref={this.cityListComponent}
+              width={width}
+              height={height}
+              rowCount={this.state.cityIndex.length}
+              rowHeight={this.getRowHeight}
+              rowRenderer={this.rowRenderer}
+              onRowsRendered={this.onRowsRendered}
+              scrollToAlignment="start"
+            />
+          )}
+        </AutoSizer>
+        {/* 右侧索引列表 */}
+        <ul className="city-index">{this.renderCityIndex()}</ul>
       </div>
     )
   }
